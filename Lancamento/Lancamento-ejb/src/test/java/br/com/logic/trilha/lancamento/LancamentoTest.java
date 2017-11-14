68f163568f78cc02cf5e7fb8fc73deaebae1c0fa
Douglas Prado <douglas.prado@logicsp.com.br>
2017-11-14 13:55:45 -0200

    Limpando projeto

--- Lancamento/Lancamento-ejb/src/test/java/br/com/logic/trilha/lancamento/LancamentoTest.java
+++ Lancamento/Lancamento-ejb/src/test/java/br/com/logic/trilha/lancamento/LancamentoTest.java
@@ -9,20 +9,23 @@ import br.com.logic.trilha.daos.LancamentoDAO;
 import br.com.logic.trilha.models.Lancamento;
 import java.util.List;
 import javax.ejb.EJB;
+import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Assert;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  *
  * @author douglas
  */
+@RunWith(Arquillian.class)
 public class LancamentoTest {
 
     @EJB
     LancamentoDAO lancamentoDAO;
 
-//    @Test
+    @Test
     public void testBuscar() {
         List<Lancamento> lista = lancamentoDAO.buscar();
         Assert.assertNotNull(lista);
